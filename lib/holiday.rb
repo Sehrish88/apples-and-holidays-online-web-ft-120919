2d0b6156a3f0d356fdda0acc043ff43b6bd6db58
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-15 20:31:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,9 @@ holiday_supplies = {
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
  
-
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
 
   # return the second element in the 4th of July array
 end
